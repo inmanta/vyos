4a6f12cb76520755d110f3e6fe3bfb646e719651
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2017-10-06 14:20:37 +0200

    Make more robust

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,14 +1,15 @@
 import re
 
 from inmanta.resources import resource, PurgeableResource, Resource
-from inmanta.agent.handler import provider, CRUDHandler, HandlerContext, cache, ResourcePurged
+from inmanta.agent.handler import provider, CRUDHandler, HandlerContext, cache, ResourcePurged, SkipResource
 
 import vymgmt
 import vyattaconfparser
+import pexpect
 
 @resource("vyos::Config", id_attribute="nodeid", agent="device")
 class Config(PurgeableResource):
-    fields = ("node", "config", "credential", "never_delete")
+    fields = ("node", "config", "credential", "never_delete", "save")
 
     @staticmethod
     def get_credential(_, obj):
@@ -31,26 +32,35 @@ class Config(PurgeableResource):
 
 @provider("vyos::Config", name="sshconfig")
 class VyosHandler(CRUDHandler): 
-    @cache(for_version=True)
+    #@cache(for_version=True)
     def get_connection(self, version, resource):
         cred = resource.credential
         vyos = vymgmt.Router(cred["address"], cred["user"], cred["password"], cred["port"])
         try:
             vyos.login()
-        except Exception as e:
-            import traceback
-            traceback.print_exc()
+        except pexpect.pxssh.ExceptionPxssh:
+            raise SkipResource("Host not available (yet)")
         return vyos
+
+    def pre(self, ctx: HandlerContext, resource: Config) -> None:
+        vyos = self.get_connection(resource.id.version, resource)
+        status = vyos._status()
+        if not status["logged_in"]:
+            try:
+                vyos.login()
+            except pexpect.pxssh.ExceptionPxssh:
+                raise SkipResource("Host not available (yet)")
 
     def post(self, ctx: HandlerContext, resource: Config) -> None:
         vyos = self.get_connection(resource.id.version, resource)
         vyos.logout()
 
-    def get_config_dict(self, vyos):
+    def get_config_dict(self, ctx, vyos):
         config = vyos.run_op_mode_command("echo 'START PRINT'; cat /config/config.boot; echo 'END PRINT'")
         config = config.replace("\r", "")
         match = re.search(r"[^\n]+\nSTART PRINT\n(.*)END PRINT\n", config, re.DOTALL)
         if match:
+            ctx.debug("Got raw config", config=match.group(1))
             conf_dict = vyattaconfparser.parse_conf(match.group(1))
             return conf_dict
 
@@ -69,14 +79,16 @@ class VyosHandler(CRUDHandler):
             vyos.set(cmd)
     
         vyos.commit()
-        vyos.save()
-        vyos.exit()
+        if resource.save:
+            vyos.save()
+        vyos.exit(force=True)
 
     def read_resource(self, ctx: HandlerContext, resource: Config) -> None:
         vyos = self.get_connection(resource.id.version, resource)
-        cfg = self.get_config_dict(vyos)
+        cfg = self.get_config_dict(ctx, vyos)
         keys = resource.node.split(" ")
         ctx.debug("Comparing desired with current", desired=resource.config, current=cfg, node=resource.node)
+
 
         for key in resource.node.split(" "):
             if isinstance(cfg, str):
@@ -97,8 +109,9 @@ class VyosHandler(CRUDHandler):
         vyos.configure()
         vyos.delete(resource.node)
         vyos.commit()
-        vyos.save()
-        vyos.exit()
+        if resource.save:
+            vyos.save()
+        vyos.exit(force=True)
         ctx.set_purged()
 
     def update_resource(self, ctx: HandlerContext, changes: dict, resource: Config) -> None:
