e58027c9b969e9b083fb9110d04c70b92b1bc1de
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2018-10-08 09:31:22 +0200

    Improve deploy

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,3 +1,10 @@
+"""
+    Vyos interface module
+
+    :copyright: 2018 Inmanta
+    :contact: code@inmanta.com
+    :license: Inmanta EULA
+"""
 import re
 
 from inmanta.resources import resource, PurgeableResource, Resource
@@ -9,7 +16,7 @@ import pexpect
 
 @resource("vyos::Config", id_attribute="nodeid", agent="device")
 class Config(PurgeableResource):
-    fields = ("node", "config", "credential", "never_delete", "save")
+    fields = ("node", "config", "credential", "never_delete", "save", "keys_only", "ignore_keys", "device")
 
     @staticmethod
     def get_credential(_, obj):
@@ -32,6 +39,10 @@ class Config(PurgeableResource):
 
 @provider("vyos::Config", name="sshconfig")
 class VyosHandler(CRUDHandler):
+    def __init__(self, agent, io=None):
+        CRUDHandler.__init__(self, agent, io)
+        self.__cache = {}
+
     #@cache(for_version=True)
     def get_connection(self, version, resource):
         cred = resource.credential
@@ -55,14 +66,87 @@ class VyosHandler(CRUDHandler):
         vyos = self.get_connection(resource.id.version, resource)
         vyos.logout()
 
-    def get_config_dict(self, ctx, vyos):
+    def get_config_dict(self, ctx, resource, vyos):
+        if resource.device in self.__cache:
+            ctx.debug("Get raw config from cache")
+            return self.__cache[resource.device]
+
         config = vyos.run_op_mode_command("echo 'START PRINT'; cat /config/config.boot; echo 'END PRINT'")
         config = config.replace("\r", "")
         match = re.search(r"[^\n]+\nSTART PRINT\n(.*)END PRINT\n", config, re.DOTALL)
         if match:
             ctx.debug("Got raw config", config=match.group(1))
             conf_dict = vyattaconfparser.parse_conf(match.group(1))
+            self.__cache[resource.device] = conf_dict
             return conf_dict
+
+    def _invalidate_cache(self, resource):
+        if resource.device in self.__cache:
+            del self.__cache[resource.device + "_" + resource.id.version]
+
+    def _dict_to_path(self, node, dct):
+        paths = []
+        for k, v in dct.items():
+            if isinstance(v, str):
+                paths.append((node + " " + k, v))
+            elif isinstance(v, dict) and len(v) == 0:
+                paths.append((node, k))
+            else:
+                paths.extend(self._dict_to_path(node + " " + k, v))
+
+        return paths
+
+    def _dict_diff(self, ignore, old, new):
+        old_keys = old.keys()
+        new_keys = new.keys()
+
+        allkeys = old_keys | new_keys
+
+        def is_diff(k):
+            if k not in old:
+                return True
+            if k not in new:
+                return not ignore
+            if new[k] is None:
+                return False
+            return old[k] != new [k]
+
+        def get_old_value(k):
+            if k not in old:
+                return None
+            return old[k]
+
+        def get_new_value(k):
+            if k not in new:
+                return None
+            value = new[k]
+            return value
+
+        changed = {k:{"current": get_old_value(k), "desired":get_new_value(k)} for k in allkeys if is_diff(k)}
+
+        return changed
+
+    def _diff(self, current, desired):
+        """
+            Generate a similar tree
+        """
+        dcfg = {}
+
+        for line in desired.config.split("\n"):
+            parts = line.split(" ")
+            if line != current.node and len(line.strip()) > 0:
+                key = " ".join(parts[:-1])
+                value = parts[-1]
+                dcfg[key] = value
+
+        ccfg = {}
+        for k, v in current.config:
+            if k not in current.ignore_keys and (len(current.keys_only) == 0 or k in current.keys_only):
+                ccfg[k] = v
+
+        changed = self._dict_diff(False, ccfg, dcfg)
+
+        return changed
 
     def _execute(self, ctx: HandlerContext, resource: Config, delete: bool) -> None:
         commands = [x for x in resource.config.split("\n") if len(x) > 0]
@@ -88,11 +172,10 @@ class VyosHandler(CRUDHandler):
 
     def read_resource(self, ctx: HandlerContext, resource: Config) -> None:
         vyos = self.get_connection(resource.id.version, resource)
-        cfg = self.get_config_dict(ctx, vyos)
+        current = self.get_config_dict(ctx, resource, vyos)
         keys = resource.node.split(" ")
-        ctx.debug("Comparing desired with current", desired=resource.config, current=cfg, node=resource.node)
 
-
+        cfg = current
         for key in resource.node.split(" "):
             if isinstance(cfg, str):
                 pass
@@ -101,13 +184,25 @@ class VyosHandler(CRUDHandler):
             else:
                 raise ResourcePurged()
 
-        resource.config = ""
+        ctx.debug("Comparing desired with current", desired=resource.config, current=cfg, node=resource.node, raw_current=current)
+
+        current_cfg = self._dict_to_path(resource.node, cfg)
+        ctx.debug("Current paths", path=current_cfg)
+        resource.config = current_cfg
 
     def create_resource(self, ctx: HandlerContext, resource: Config) -> None:
+        ctx.debug("Creating resource, invalidating cache")
+        if resource.device in self.__cache:
+            del self.__cache[resource.device]
+
         self._execute(ctx, resource, delete=False)
         ctx.set_created()
 
     def delete_resource(self, ctx: HandlerContext, resource: Config) -> None:
+        ctx.debug("Deleting resource, invalidating cache")
+        if resource.device in self.__cache:
+            del self.__cache[resource.device]
+
         vyos = self.get_connection(resource.id.version, resource)
         vyos.configure()
         vyos.delete(resource.node)
@@ -118,5 +213,9 @@ class VyosHandler(CRUDHandler):
         ctx.set_purged()
 
     def update_resource(self, ctx: HandlerContext, changes: dict, resource: Config) -> None:
+        ctx.debug("Updating resource, invalidating cache")
+        if resource.device in self.__cache:
+            del self.__cache[resource.device]
+
         self._execute(ctx, resource, delete=True)
         ctx.set_updated()
