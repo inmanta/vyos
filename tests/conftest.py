720946ef0cb95cfc84c970ce1a6e5d9815b0586e
Wouter De Borger <wouter.deborger@inmanta.com>
2019-10-30 15:08:33 +0100

    added ospf

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
 from pytest import fixture
 import os
+import vymgmt
 
 @fixture
 def vy_host():
     return os.environ["VY_TEST_HOST"]
+
+
+@fixture
+def console(vy_host):
+    vyos = vymgmt.Router(vy_host, "vyos", "vyos", 22)
+    vyos.login()
+    yield vyos
+    vyos.logout()
