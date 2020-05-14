b6dbabb8e36ff7fd47bf2557a32546e9ed8a4dc9
Sander Van Balen <git@sandervanbalen.be>
2020-05-14 15:40:28 +0200

    test fixes

--- tests/test_route.py
+++ tests/test_route.py
@@ -1,3 +1,8 @@
+from typing import Iterable, Optional
+
+import pytest
+
+
 def convert_bool(val):
     return "true" if val else "false"
 
@@ -47,7 +52,28 @@ def test_static_route(project, vy_host, clear):
     assert len(compare) == 0
 
 
-def test_policy_route(project, vy_host, clear):
+@pytest.mark.parametrize(
+    "source_is_set,dest_is_set,protocol_is_set,description_is_set",
+    [
+        (False, False, False, False),
+        (True, False, False, False),
+        (True, True, False, False),
+        (True, True, True, False),
+        (True, True, True, True),
+    ],
+)
+def test_policy_route(
+    project,
+    vy_host,
+    clear,
+    source_is_set: bool,
+    dest_is_set: bool,
+    protocol_is_set: bool,
+    description_is_set: bool,
+) -> None:
+    def unlines_optional(lines: Iterable[Optional[str]]) -> str:
+        return "\n".join(line for line in lines if line is not None)
+
     def make_config(purge=False):
         project.compile(
             f"""
@@ -65,26 +91,36 @@ def test_policy_route(project, vy_host, clear):
         name = "T2",
         rule = 1,
         table = 2,
-        source_address = "192.168.100.104/29",
-        protocol = "tcp",
-        description = "my description",
+        %s
         purged = {convert_bool(purge)}
     )
-            """,
+            """
+            % ",".join(
+                line
+                for line in [
+                    "source_address = '192.168.100.104/29'" if source_is_set else None,
+                    "destination_address = '192.168.2.2/29'" if dest_is_set else None,
+                    "protocol = 'tcp'" if protocol_is_set else None,
+                    "description = 'my description'" if description_is_set else None,
+                    # make sure trailing comma is added when at least one line present
+                    "",
+                ]
+                if line is not None
+            ),
         )
 
     make_config()
 
-    assert (
-        project.get_resource("vyos::Config").config.strip()
-        == (
-            """
-policy route T2 rule 1 set table '2'
-policy route T2 rule 1 source address '192.168.100.104/29'
-policy route T2 rule 1 protocol 'tcp'
-policy route T2 rule 1 description 'my description'
-        """
-        ).strip()
+    assert project.get_resource("vyos::Config").config.strip() == "\n".join(
+        f"policy route T2 rule 1 {line}"
+        for line in [
+            "set table '2'",
+            "source address '192.168.100.104/29'" if source_is_set else None,
+            "destination address '192.168.2.2/29'" if dest_is_set else None,
+            "protocol 'tcp'" if protocol_is_set else None,
+            "description 'my description'" if description_is_set else None,
+        ]
+        if line is not None
     )
 
     compare = project.dryrun_resource("vyos::Config")
@@ -94,6 +130,7 @@ policy route T2 rule 1 description 'my description'
     project.deploy_resource("vyos::Config")
 
     compare = project.dryrun_resource("vyos::Config")
+    print(compare)
     assert len(compare) == 0
 
     make_config(purge=True)
