36c55677c32ee0f395cb8adc15e67d31a6e9c7ed
Sander Van Balen <git@sandervanbalen.be>
2020-05-14 14:32:49 +0200

    added draft test case

--- tests/test_route.py
+++ tests/test_route.py
@@ -45,3 +45,57 @@ def test_static_route(project, vy_host, clear):
 
     compare = project.dryrun_resource("vyos::Config")
     assert len(compare) == 0
+
+
+def test_policy_route(project, vy_host, clear):
+    def make_config(purge=False):
+        project.compile(
+            f"""
+    import vyos
+
+    r1 = vyos::Host(
+        name = "lab1",
+        user = "vyos",
+        password = "vyos",
+        ip = "{vy_host}",
+    )
+
+    vyos::PolicyRoute(
+        host = r1,
+        name = "T2",
+        rule = 1,
+        table = 2,
+        source_address = "192.168.100.104/29",
+        protocol = "tcp",
+        description = "my description",
+        purged = {convert_bool(purge)}
+    )
+            """,
+        )
+
+    make_config()
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert "policy route T2 rule 1 set table" in compare
+    assert "policy route T2 rule 1 source address" in compare
+    assert "policy route T2 rule 1 destination address" in compare
+    assert "policy route T2 rule 1 protocol" in compare
+    assert "policy route T2 rule 1 description" in compare
+    assert "purged" in compare
+    assert len(compare) == 6
+
+    project.deploy_resource("vyos::Config")
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert len(compare) == 0
+
+    make_config(purge=True)
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert "purged" in compare
+    assert len(compare) == 1
+
+    project.deploy_resource("vyos::Config")
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert len(compare) == 0
