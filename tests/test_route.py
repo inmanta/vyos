599894dc4466c17b82c39e96ca8734e50410ca91
Sander Van Balen <git@sandervanbalen.be>
2020-05-14 16:34:12 +0200

    test fix

--- tests/test_route.py
+++ tests/test_route.py
@@ -102,7 +102,7 @@ def test_policy_route(
                     "source_address = '192.168.100.104/29'" if source_is_set else None,
                     "destination_address = '192.168.2.2/29'" if dest_is_set else None,
                     "protocol = 'tcp'" if protocol_is_set else None,
-                    "description = 'my_description'" if description_is_set else None,
+                    "description = 'MyDescription'" if description_is_set else None,
                     # make sure trailing comma is added when at least one line present
                     "",
                 ]
@@ -119,7 +119,7 @@ def test_policy_route(
             "source address 192.168.100.104/29" if source_is_set else None,
             "destination address 192.168.2.2/29" if dest_is_set else None,
             "protocol tcp" if protocol_is_set else None,
-            "description my_description" if description_is_set else None,
+            "description MyDescription" if description_is_set else None,
         ]
         if line is not None
     )
