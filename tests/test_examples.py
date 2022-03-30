cf741882246c791276de69c41ef93a5dfe955ca2
Wouter De Borger <wouter.deborger@inmanta.com>
2022-03-30 13:01:59 +0200

    fix pep8

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,9 +1,11 @@
 import os
 
+
 def load_example(example, ip):
-    path = os.path.join(os.path.dirname(__file__),f"../examples/{example}.cf")
+    path = os.path.join(os.path.dirname(__file__), f"../examples/{example}.cf")
     with open(path, mode="r") as fh:
         return fh.read().replace('mgmt_ip = "x.x.x.x"', f'mgmt_ip = "{ip}"')
+
 
 def test_example_interfaces(project, vyos_device):
     project.compile(load_example("interfaces", vyos_device))
