8e189896237f2b61bd40c0d870dd945ebe5f5b48
Pouriya <pouriya.jamshidi@inmanta.com>
2022-05-12 12:23:35 +0200

    add OSPFv3 support

---
+++ tests/test_ospfv3.py
@@ -0,0 +1,48 @@
+def convert_bool(val):
+    return "true" if val else "false"
+
+
+def test_ospf(project, vy_host, clear):
+    def make_config(purge=False):
+        project.compile(
+            f"""
+    import vyos
+
+    r1 = vyos::Host(
+        name="lab1",
+        user="vyos",
+        password="vyos",
+        ip="{vy_host}"
+    )
+
+    ospf1 = vyos::Ospfv3(
+        area=0,
+        interfaces=["eth1"],
+        router_id="10.10.10.1",
+        host=r1,
+        purged={convert_bool(purge)}
+    )
+        """
+        )
+
+    make_config()
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert "purged" in compare
+    assert len(compare) == 1
+
+    project.deploy_resource("vyos::Config")
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert len(compare) == 0
+
+    make_config(True)
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert "purged" in compare
+    assert len(compare) == 1
+
+    project.deploy_resource("vyos::Config")
+
+    compare = project.dryrun_resource("vyos::Config")
+    assert len(compare) == 0
